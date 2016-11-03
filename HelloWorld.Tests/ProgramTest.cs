788836c9477807a42631f9118f6088093081eaa8
Oussama MARINE <oussama.marine@gmail.com>
2016-11-03 15:08:02 +0100

    Add Testin project

---
+++ HelloWorld.Tests/ProgramTest.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace HelloWorld.Tests
+{
+    [TestClass]
+    public class ProgramTest
+    {
+
+        [TestMethod]
+        public void TestCreateMessage()
+        {
+            Assert.AreEqual("Hello World", Program.CreateMessage());
+        }
+
+
+    }
+}
