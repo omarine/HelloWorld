e0a0d3bc99c439c6af03ff99da3a661e6b1adc5d
Oussama MARINE <oussama.marine@gmail.com>
2016-11-04 13:36:40 +0100

    Bad code

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HelloWorld
 {
@@ -15,7 +11,7 @@ namespace HelloWorld
 
         public static string CreateMessage()
         {
-            return "Hello World";
+            return "Hello World!";
         }
 
     }
